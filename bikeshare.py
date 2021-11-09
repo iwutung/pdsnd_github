46b189b7f6031ebc003a6da9d1efb3cffa95f946
Toliva-Solomon-Iwutung <tisopar@gmail.com>
2021-11-09 10:43:34 +0300

    refactor: Remove unnecessary .keys() method in get_filters()

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input('Please select a city to explore.\n Chicago\n New York\n Washington\n -> ').lower()
-        if city in CITY_DATA.keys():
+        if city in CITY_DATA:
             break
         else:
             print('Oops... looks like an error occured! \nPlease enter a valid option! example: chicago')
